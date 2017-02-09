0b27872260087b8da8d71ad0fa4f2177ea57b25e
Equim <sayaka@equimcute.com>
2017-02-09 18:59:50 +0800

    Minor format

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 'use strict';
 
-const
-    express    = require('express'),
-    superagent = require('superagent'),
-    cheerio    = require('cheerio'),
-    colors     = require('colors'),
-    program    = require('commander'),
-    moment     = require('moment'),
-    co         = require('co'),
-    thunkify   = require('thunkify'),
-    access     = require('./lib/access.js');
+const express    = require('express'),
+      superagent = require('superagent'),
+      cheerio    = require('cheerio'),
+      colors     = require('colors'),
+      program    = require('commander'),
+      moment     = require('moment'),
+      co         = require('co'),
+      thunkify   = require('thunkify'),
+      access     = require('./lib/access.js');
 
 program
     .option('-h, --help')
@@ -18,7 +17,6 @@ program
     .option('-f, --fullLog')
     .parse(process.argv);
 
-// 别问我为什么这里逻辑这么奇怪……测试的结果确实是这样的啊hhh
 if (!program.help || !program.version) {
     console.log(
 `${`CSUEMS API v${require('./package').version}
@@ -46,68 +44,71 @@ Examples:
 
 superagent.Request.prototype.endThunk = thunkify(superagent.Request.prototype.end);
 
-const
-    logging = (log) => console.log(`${moment().format('[[]YY-MM-DD HH:mm:ss[]]')} ${log}`),
-    fullLogging = (log) => program.fullLog && logging(log),
-    getSem = () => {
-        // 以系统时间获取当前学期
-        let now = new Date();
-        let month = now.getMonth();
-        let year = now.getFullYear();
-        if (month <= 6) {
-            return `${year - 1}-${year}-${month > 0 ? 2 : 1}`;
-        }
-        return `${year}-${year + 1}-1`;
-    },
-//  wait = (ms) => (callback) => setTimeout(callback, ms),
-    port = program.port || process.env.PORT || 2333,
-    app = express();
+const port = program.port || process.env.PORT || 2333,
+      app = express();
+
+const logging = (log) => console.log(`${moment().format('[[]YY-MM-DD HH:mm:ss[]]')} ${log}`);
+
+const fullLogging = (log) => program.fullLog && logging(log);
+
+// 根据系统时间计算当前学期
+const getSem = () => {
+    let now = new Date();
+    let month = now.getMonth();
+    let year = now.getFullYear();
+    return month <= 6 ? `${year - 1}-${year}-${month > 0 ? 2 : 1}` : `${year}-${year + 1}-1`;
+};
 
 // 获取文档
 app.get('/doc', (req, res) => res.sendFile(`${__dirname}/doc/API.html`));
 
 // 查成绩API，通过GET传入用户名和密码
 app.get(/^\/g(?:|rades)$/, co.wrap(function *(req, res) {
+    // 检查参数
     if (!req.query.id || !req.query.pwd || (req.query.sem && !(/^20\d{2}-20\d{2}-[1-2]$/).test(req.query.sem))) {
-        res.status(404).send({ error: "参数不正确" });
+        res.status(404).json({ error: "参数不正确" });
         return;
     }
 
+    // 记录处理用时
     let start = new Date();
     fullLogging('Started to query the grades: '.cyan + req.query.id.yellow);
 
+    // 登录，获取headers
     let headers;
     try {
         headers = yield access.login(req.query.id, req.query.pwd);
     } catch (errMsg) {
         logging(errMsg.inner.red);
-        res.status(404).send({ error: errMsg.public });
+        res.status(404).json({ error: errMsg.public });
         return;
     }
     fullLogging('Successfully logged in.'.green);
 
+    // 进入成绩页面
     let ires;
     try {
         // 实际上xnxq01id为空的时候和GET这个URL的效果是一样的，都是查询所有学期
         ires = yield superagent
-            .post('http://csujwc.its.csu.edu.cn/jsxsd/kscj/yscjcx_list')
-            .set(headers)
-            .type('form')
-            .send({ xnxq01id: req.query.sem })
-            .endThunk();
+                         .post('http://csujwc.its.csu.edu.cn/jsxsd/kscj/yscjcx_list')
+                         .set(headers)
+                         .type('form')
+                         .send({ xnxq01id: req.query.sem })
+                         .endThunk();
     } catch (err) {
         logging(`Failed to get grades page\n${err.stack}`.red);
-        res.status(404).send({ error: '无法进入成绩页面' });
+        res.status(404).json({ error: '无法进入成绩页面' });
         return;
     } finally {
-        // 直接异步进行?
+        // 异步登出
         co(function *() {
             try {
                 yield access.logout(headers);
-                fullLogging('Successfully logged out: '.green + req.query.id.yellow);
             } catch (errMsg) {
                 logging(errMsg.inner.red);
+                return;
             }
+            fullLogging('Successfully logged out: '.green + req.query.id.yellow);
         });
     }
     fullLogging('Successfully entered grades page.'.green);
@@ -163,14 +164,14 @@ app.get(/^\/g(?:|rades)$/, co.wrap(function *(req, res) {
     result['subject-count'] = Object.keys(result.grades).length;
     result['failed-count'] = Object.keys(result.failed).length;
 
-    res.send(JSON.stringify(result));
-    fullLogging(`Successfully responded. (req -> res processed in ${new Date() - start} ms)`.green);
+    res.json(result);
+    fullLogging(`Successfully responded. (req -> res processed in ${new Date() - start}ms)`.green);
 }));
 
 // 查考试API，通过GET传入用户名和密码
 app.get(/^\/e(?:|xams)$/, co.wrap(function *(req, res) {
     if (!req.query.id || !req.query.pwd || (req.query.sem && !(/^20\d{2}-20\d{2}-[1-2]$/).test(req.query.sem))) {
-        res.status(404).send({ error: "参数不正确" });
+        res.status(404).json({ error: "参数不正确" });
         return;
     }
 
@@ -182,7 +183,7 @@ app.get(/^\/e(?:|xams)$/, co.wrap(function *(req, res) {
         headers = yield access.login(req.query.id, req.query.pwd);
     } catch (errMsg) {
         logging(errMsg.inner.red);
-        res.status(404).send({ error: errMsg.public });
+        res.status(404).json({ error: errMsg.public });
         return;
     }
     fullLogging('Successfully logged in.'.green);
@@ -191,18 +192,18 @@ app.get(/^\/e(?:|xams)$/, co.wrap(function *(req, res) {
     let _sem = req.query.sem || getSem();
     try {
         ires = yield superagent
-            .post('http://csujwc.its.csu.edu.cn/jsxsd/xsks/xsksap_list')
-            .set(headers)
-            .type('form')
-            .send({
-                xqlbmc: '',
-                xnxqid: _sem,
-                xqlb: ''
-            })
-            .endThunk();
+                         .post('http://csujwc.its.csu.edu.cn/jsxsd/xsks/xsksap_list')
+                         .set(headers)
+                         .type('form')
+                         .send({
+                             xqlbmc: '',
+                             xnxqid: _sem,
+                             xqlb: ''
+                         })
+                         .endThunk();
     } catch (err) {
         logging(`Failed to reach exams page\n${err.stack}`.red);
-        res.status(404).send({ error: '无法进入考试页面' });
+        res.status(404).json({ error: '无法进入考试页面' });
         return;
     } finally {
         co(function *() {
@@ -244,8 +245,8 @@ app.get(/^\/e(?:|xams)$/, co.wrap(function *(req, res) {
         result['exams-count']++;
     });
 
-    res.send(JSON.stringify(result));
-    fullLogging(`Successfully responded. (req -> res processed in ${new Date() - start} ms)`.green);
+    res.json(result);
+    fullLogging(`Successfully responded. (req -> res processed in ${new Date() - start}ms)`.green);
 }));
 
 app.listen(port, () => {
